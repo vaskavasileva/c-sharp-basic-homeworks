315622b50d4c016a6427599d432260b5f2e40755
vaskavasileva <61829894+vaskavasileva@users.noreply.github.com>
2020-03-09 09:53:05 +0100

    Homework3

---
+++ Class02/Homework03/Homework03/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Homework03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 102;
+            int m = 5;
+            int result = 102 / 5;
+            Console.WriteLine(result);
+        }
+    }
+}

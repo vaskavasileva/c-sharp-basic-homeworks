cad4c1c5dc4ad5f237ff3e41468eb0d15c1b3d94
vaskavasileva <61829894+vaskavasileva@users.noreply.github.com>
2020-03-09 09:47:02 +0100

    Homework1

---
+++ Class02/Homework01/Homework01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Homework01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Vaska";
+            string lastName = "Vasileva";
+            string name = firstName + " " + lastName;
+
+            string number1 = "9";
+            string number2 = "3";
+            int num1 = Convert.ToInt32(number1);
+            int num2 = Convert.ToInt32(number2);
+            int summ = num1 + num2;
+
+            Console.WriteLine("My name is: " + name);
+            Console.WriteLine("The summ of the numbers is: " + summ);
+
+        }
+    }
+}

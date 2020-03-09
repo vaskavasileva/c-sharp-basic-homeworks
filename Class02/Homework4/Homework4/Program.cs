1b49e852a36549252ae16d689c11f0980297bd69
vaskavasileva <61829894+vaskavasileva@users.noreply.github.com>
2020-03-09 10:10:22 +0100

    Homework4

---
+++ Class02/Homework4/Homework4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number : ");
+            var input = Console.ReadLine();
+            int number;
+            bool output = int.TryParse(input, out number);
+            if (output == true)
+
+            {
+                if (number % 2 == 0)
+                    Console.WriteLine("The number is even.");
+                else
+                    Console.WriteLine("The number is odd.");
+            }
+
+            else
+                Console.WriteLine("Invalid input.");
+            Console.ReadLine();
+        }
+    }
+}

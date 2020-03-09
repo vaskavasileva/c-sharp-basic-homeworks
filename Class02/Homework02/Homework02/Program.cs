07295d8945ea68c45a369bdb38a6d9cfd459d69f
vaskavasileva <61829894+vaskavasileva@users.noreply.github.com>
2020-03-09 09:51:09 +0100

    Homework2

---
+++ Class02/Homework02/Homework02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Homework02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+            string input = "9";
+            int number1 = Convert.ToInt32(input);
+
+            int result = number1 - number;
+            Console.WriteLine("The result is: " + result);
+        }
+    }
+}

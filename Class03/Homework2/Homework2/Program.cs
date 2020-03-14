64780bd76a01b5a4f3b61d2967098993756d4f68
vaskavasileva <61829894+vaskavasileva@users.noreply.github.com>
2020-03-14 13:32:35 +0100

    Homework2

---
+++ Class03/Homework2/Homework2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            var input1 = Console.ReadLine();
+            int number1 = Convert.ToInt32(input1);
+            for (int i = 2; i <= number1 ; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.Write("Enter another number: ");
+            var input2 = Console.ReadLine();
+            int number2 = Convert.ToInt32(input2);
+            for (int m = 1; m <= number2; m++)
+            {
+                if (m % 2 != 0)
+                {
+                    Console.WriteLine(m);
+                }
+            }
+        }
+    }
+}

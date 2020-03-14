6231a62521823a27d6be8988a1619caea02f1088
vaskavasileva <61829894+vaskavasileva@users.noreply.github.com>
2020-03-14 13:48:13 +0100

    Homework3

---
+++ Class03/Homework3/Homework3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new string[10];
+            int i = 0;
+            char answer = 'y';
+            do
+            {
+                Console.WriteLine("Enter a name: ");
+                names[i] = Console.ReadLine();
+
+                Console.WriteLine("Do you want to enter another name? (y/n)");
+                var input = Console.ReadLine();
+                answer = Convert.ToChar(input);
+
+            } while (answer == 'y');
+
+
+            
+        }
+    }
+}

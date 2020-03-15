270c70e8b08da92fd02b9879682e7301fb3ac468
vaskavasileva <61829894+vaskavasileva@users.noreply.github.com>
2020-03-15 18:31:33 +0100

    Extra04

---
+++ Extra/Exercise04/Exercise04/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercise04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var appearances = 0;
+            var arrayWithDuplicates = new int[] { 1, 4, 6, 3, 4, 5, 9, 3, 2, 9 };
+
+            for (int i = 0; i < arrayWithDuplicates.Length; i++)
+            {
+                for (int j = i + 1; j < arrayWithDuplicates.Length; j++)
+                {
+                    if (arrayWithDuplicates[i] == arrayWithDuplicates[j])
+                    {
+                        appearances++;
+                    }
+                }
+            }
+            Console.WriteLine(appearances);
+            Console.Read();
+        }
+
+
+            
+        
+    }
+}

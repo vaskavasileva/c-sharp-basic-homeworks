1f56e4b5fbb4bafeb0b3a9be5e7d090ad8781dee
vaskavasileva <61829894+vaskavasileva@users.noreply.github.com>
2020-03-15 20:48:46 +0100

    exercise7

---
+++ Extra/Exercise07/Exercise07/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Exercise07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userNames = new string[] { "user1", "user2", "user3" };
+            var passwords = new string[] { "first", "second", "third" };
+            var check = false;
+            do
+            {
+            Console.WriteLine("Enter a username");
+            var user = Console.ReadLine();
+                if (user == userNames[0] || user == userNames[1] || user == userNames[2])
+                {
+                    for (int i = 0; i <= 2; i++)
+                    {
+                        if (user == userNames[i])
+                        {
+                            Console.WriteLine("Enter a password");
+                            var pass = Console.ReadLine();
+                            if (pass == passwords[i])
+                            {
+                                Console.WriteLine("You are logged in succesfully");
+                                check = true;
+                                break;
+                            }
+                            else
+                                Console.WriteLine("Incorrect user or password");
+
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Enter a password");
+                    Console.ReadLine();
+                    Console.WriteLine("Incorrect user or password");
+                }
+            } while (check == false);
+        }
+    }
+}

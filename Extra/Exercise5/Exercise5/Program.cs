2b5d350b2020e97e853b3e4c8b778a779bf4cf3f
vaskavasileva <61829894+vaskavasileva@users.noreply.github.com>
2020-03-15 19:15:11 +0100

    Extra5

---
+++ Extra/Exercise5/Exercise5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var oddEvenArray = new int[] { 10, 23, 44, 123, 55, 52, 98, 99, 102, 103, 152, 49 };
+            var oddArray = new int[0];
+            var evenArray = new int[0];
+            for (int i = 0; i < oddEvenArray.Length; i++)
+            {
+                if (oddEvenArray[i] % 2 == 0)
+                {
+                    Array.Resize(ref evenArray, evenArray.Length + 1);
+                    evenArray[evenArray.Length - 1] = oddEvenArray[i];
+                }
+                else
+                {
+                    Array.Resize(ref oddArray, oddArray.Length + 1);
+                    oddArray[oddArray.Length - 1] = oddEvenArray[i];
+                }
+            }
+
+            Console.WriteLine("Nizata parni broevi gi ima clenovite: ");
+            foreach (var number in evenArray)
+            {
+                    Console.WriteLine(number);
+            }
+
+            Console.WriteLine("Nizata neparni broevi gi ima clenovite: ");
+            foreach (var number in oddArray)
+            {
+                    Console.WriteLine(number);
+            }
+        }
+    }
+}

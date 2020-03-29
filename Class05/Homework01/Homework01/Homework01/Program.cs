1b42d56bf6eaf490b7c05ee4ca1f22e8d433b726
vaskavasileva <61829894+vaskavasileva@users.noreply.github.com>
2020-03-29 18:54:58 +0200

    Homework1

---
+++ Class05/Homework01/Homework01/Homework01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Homework01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string you want to reverse: ");
+            string input = Console.ReadLine();
+            string copy = input;
+            var characters = input.ToCharArray();
+            Array.Reverse(characters);
+            Console.WriteLine("The reversed order of characters in the string {0} is : ", copy);
+            foreach (var character in characters)
+            {
+                Console.WriteLine(character);
+            }
+
+        }
+    }
+}

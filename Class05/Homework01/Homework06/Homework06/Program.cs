0bf1ccf8f79541655077dfeb5b6b0e08cfc20aaa
vaskavasileva <61829894+vaskavasileva@users.noreply.github.com>
2020-03-29 20:27:21 +0200

    Homework6

---
+++ Class05/Homework01/Homework06/Homework06/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Homework06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2008; i <= 2020; i++)
+            {
+                if (DateTime.IsLeapYear(i))
+                {
+                    Console.WriteLine(i + " is a leap year!");
+                }
+            }
+        }
+    }
+}

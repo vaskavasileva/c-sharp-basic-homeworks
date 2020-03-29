05de640b0934e7c5872c175c2862f0a8d53e222b
vaskavasileva <61829894+vaskavasileva@users.noreply.github.com>
2020-03-29 18:59:49 +0200

    Homework 2

---
+++ Class05/Homework01/Homework02/Homework02/Homework02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Homework02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");
+            var input = Console.ReadLine();
+            var words = input.Split(' ');
+            int count = 0;
+            foreach (var word in words)
+            {
+                count++;
+            }
+            Console.WriteLine("The number of words in your sentence is: " + count);
+        }
+    }
+}

5a06f8d61949e4cf93de4401e84818a22e8f5c4b
vaskavasileva <61829894+vaskavasileva@users.noreply.github.com>
2020-03-29 20:16:42 +0200

    Homework04

---
+++ Class05/Homework01/Homework04/Homework04/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Homework04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "The whole group of G1 loves C#, we are sure that with their deducation and passion they will be successfull programers soon!";
+            Console.WriteLine(sentence.Substring(sentence.IndexOf(',')+1)); 
+        }
+    }
+}

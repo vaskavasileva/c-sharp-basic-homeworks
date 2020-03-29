b86cb03f6163d077a29f732dc2c6861e7e13765d
vaskavasileva <61829894+vaskavasileva@users.noreply.github.com>
2020-03-29 20:23:22 +0200

    Homework05

---
+++ Class05/Homework01/Homework05/Homework05/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Homework05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine("year:" + now.Year);
+            Console.WriteLine("month:" + now.Month);
+            Console.WriteLine("day:" + now.Day);
+            Console.WriteLine("hour:" + now.Hour);
+            Console.WriteLine("minute:" + now.Minute);
+            Console.WriteLine("second:" + now.Second);
+        }
+    }
+}

66a432b1cb49b00e59e3f460464e8b57f665ddc3
vaskavasileva <61829894+vaskavasileva@users.noreply.github.com>
2020-04-01 01:18:53 +0200

    Homeworks

---
+++ Class06/Homework01/Homework01/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Homework01
+{
+    class PhotoAlbum
+    {
+        public PhotoAlbum()
+        {
+            numberOfPages = 16;
+        }
+
+        public PhotoAlbum(int specificNumberOfPages)
+        {
+            numberOfPages = specificNumberOfPages;
+        }
+
+        private int numberOfPages;
+
+        public void GetNumberOfPages()
+        {
+            Console.WriteLine(numberOfPages);
+        }
+
+    }
+
+    class BigPhotoAlbum
+    {
+        public BigPhotoAlbum()
+        {
+            numberOfPages = 64;
+        }
+
+        private int numberOfPages;
+
+        public void GetNumberOfPages()
+        {
+            Console.WriteLine(numberOfPages);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var photoAlbum1 = new PhotoAlbum();
+            Console.Write($"The number of pages in the deafult photo album is: ");
+            photoAlbum1.GetNumberOfPages();
+
+            var photoAlbum2 = new PhotoAlbum(24);
+            Console.Write($"The number of pages in the custom photo album is: ");
+            photoAlbum2.GetNumberOfPages();
+
+            var bigPhotoAlbum = new BigPhotoAlbum();
+            Console.Write($"The number of pages in the big photo album is: ");
+            bigPhotoAlbum.GetNumberOfPages();
+
+        }
+    }
+}
---
+++ Class06/Homework02/Homework02/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace Homework02
+{
+    public enum Registration
+    {
+        passed = 1,
+        notPassed
+    }
+
+    public enum Grades
+    {
+        Failure = 5,
+        LowPass,
+        Satisfactory,
+        Good,
+        Superior,
+        Outstanding
+    }
+
+    class FirstCourse
+    {
+        public FirstCourse()
+        {
+
+        }
+        public FirstCourse(string name, Registration didStudentPass)
+        {
+            CourseName = name;
+            registration = didStudentPass;
+        }
+
+        public string CourseName;
+        public Registration registration;
+
+        public int Passed()
+        {
+            if (registration == Registration.passed)
+            {
+                return 1;
+            }
+
+            else
+            {
+                return 0;
+            }
+        }
+    }
+
+    class SecondCourse
+    {
+        public SecondCourse(string name, Grades studentGrade)
+        {
+            CourseName = name;
+            grade = studentGrade;
+        }
+
+        public string CourseName;
+        public Grades grade;
+
+
+        public int Passed()
+        {
+            if (grade == Grades.Failure)
+            {
+                return 0;
+            }
+
+            else 
+            {
+                return 1;
+            }
+        }
+
+        
+    }
+
+    class Project
+    {
+
+        public int[] ifPassed;
+        public int sum = 0;
+
+        public void Passed()
+        {
+            foreach (var eval in ifPassed)
+            {
+                sum += eval;
+            }
+
+            if (sum >= 3)
+            {
+                Console.WriteLine("The student has passed!");
+            }
+            else
+            {
+                Console.WriteLine("The student has failed!");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstCourse1 = new FirstCourse("C#", Registration.passed);
+            var firstCourse2 = new FirstCourse("C# Advanced", Registration.passed);
+            var secondCourse1 = new SecondCourse("ASP.NET", Grades.Failure);
+            var secondCourse2 = new SecondCourse("C# Server Development", Grades.Outstanding);
+
+            var project = new Project
+            {
+                ifPassed = new int[4]
+                {
+                firstCourse1.Passed(),
+                firstCourse2.Passed(),
+                secondCourse1.Passed(),
+                secondCourse2.Passed()
+                }
+            };
+
+            project.Passed();
+
+        }
+
+        
+        
+    }
+}
+
+
